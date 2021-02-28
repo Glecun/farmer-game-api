39d11ed8a922490ba269b264c732dc18fad90506
Glecun <gregoire.lecun@gmail.com>
2021-02-28 22:35:00 +0100

    First Commit

---
+++ src/main/java/com/glecun/farmergameapi/FarmerGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.glecun.farmergameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerGameApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/glecun/farmergameapi/application/StatusResource.java
@@ -0,0 +1,12 @@
+package com.glecun.farmergameapi.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusResource {
+    @GetMapping("/status")
+    public String ping() {
+        return "RUNNING";
+    }
+}
