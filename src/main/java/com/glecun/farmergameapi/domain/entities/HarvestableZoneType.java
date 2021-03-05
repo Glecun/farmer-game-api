4c34666def97b78157c20f99a58ce65873933f88
Glecun <gregoire.lecun@gmail.com>
2021-03-06 00:38:50 +0100

    Update HarvestableZone nbOfZones

--- src/main/java/com/glecun/farmergameapi/domain/entities/HarvestableZoneType.java
+++ src/main/java/com/glecun/farmergameapi/domain/entities/HarvestableZoneType.java
@@ -1,9 +1,9 @@
 package com.glecun.farmergameapi.domain.entities;
 
 public enum HarvestableZoneType {
-    ZONE_1(false, 9),
-    ZONE_2(false, 9),
-    ZONE_3(true, 27);
+    ZONE_1(false, 12),
+    ZONE_2(false, 12),
+    ZONE_3(true, 40);
 
     public final boolean lockedByDefaut;
     public final int nbOfZone;
