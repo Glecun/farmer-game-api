e66aca97eff22f6329ecfe9ec19ddb3519a6ae50
Glecun <gregoire.lecun@gmail.com>
2021-03-12 20:00:00 +0100

    Add log for cron

--- src/main/java/com/glecun/farmergameapi/application/CronByGrowthTime.java
+++ src/main/java/com/glecun/farmergameapi/application/CronByGrowthTime.java
@@ -2,12 +2,16 @@ package com.glecun.farmergameapi.application;
 
 import com.glecun.farmergameapi.domain.ApplicationDomain;
 import com.glecun.farmergameapi.domain.entities.GrowthTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CronByGrowthTime {
+
+    Logger logger = LoggerFactory.getLogger(CronByGrowthTime.class);
 
     private final ApplicationDomain applicationDomain;
 
@@ -20,11 +24,13 @@ public class CronByGrowthTime {
 
     @Scheduled(fixedRate = 20000, initialDelay = 5000)
     public void firstGrowthTime() {
+        logger.info("Start firstGrowthTime");
         applicationDomain.resolveSales(GrowthTime.FIRST_GROWTH_TIME);
     }
 
     @Scheduled(fixedRate = 120000, initialDelay = 55000)
     public void secondGrowthTime() {
+        logger.info("Start secondGrowthTime");
         applicationDomain.resolveSales(GrowthTime.SECOND_GROWTH_TIME);
     }
 
--- src/main/java/com/glecun/farmergameapi/application/CronByMinGrowthTime.java
+++ src/main/java/com/glecun/farmergameapi/application/CronByMinGrowthTime.java
@@ -1,5 +1,7 @@
 package com.glecun.farmergameapi.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,7 @@ import com.glecun.farmergameapi.domain.entities.GrowthTime;
 @Component
 public class CronByMinGrowthTime {
 
+   Logger logger = LoggerFactory.getLogger(CronByMinGrowthTime.class);
 
    private final ApplicationDomain applicationDomain;
 
@@ -19,11 +22,13 @@ public class CronByMinGrowthTime {
 
    @Scheduled(fixedRate = 20000)
    public void firstMinGrowthTime() {
+      logger.info("Start firstMinGrowthTime");
       applicationDomain.generateMarketInfos(GrowthTime.FIRST_GROWTH_TIME);
    }
 
    @Scheduled(fixedRate = 120000)
    public void secondMinGrowthTime() {
+      logger.info("Start secondMinGrowthTime");
       applicationDomain.generateMarketInfos(GrowthTime.SECOND_GROWTH_TIME);
    }
 }
